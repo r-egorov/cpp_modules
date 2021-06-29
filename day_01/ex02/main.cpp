326759e350cb4014513464d411677b64093f7652
Ruslan Egorov <r-egorov@outlook.com>
2021-06-29 17:04:31 +0300

    Add ex02

---
+++ day_01/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int		main(void)
+{
+	std::string		string = "HI THIS IS BRAIN";
+	std::string		*stringPTR = &string;
+	std::string		&stringREF = string;
+
+	std::cout << "Printing addresses" << std::endl;
+	std::cout << "&string:\t|" << &string << "|" << std::endl;
+	std::cout << "*stringPTR:\t|" << stringPTR << "|" << std::endl;
+	std::cout << "&stringREF:\t|" << &stringREF << "|" << std::endl;
+
+	std::cout << "Printing the value" << std::endl;
+	std::cout << "string:\t\t|" << string << "|" << std::endl;
+	std::cout << "*stringPTR:\t|" << *stringPTR << "|" << std::endl;
+	std::cout << "stringREF:\t|" << stringREF << "|" << std::endl;
+}
