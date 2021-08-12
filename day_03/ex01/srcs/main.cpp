dbd7d9208cc267059a46e771078b6141f66e4606
r-egorov <r-egorov@outlook.com>
2021-08-12 14:23:48 +0300

    [Day03]ex01 done

---
+++ day_03/ex01/srcs/main.cpp
@@ -0,0 +1,49 @@
+#include "ScavTrap.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	ScavTrap	def_scav;
+	ScavTrap	par_scav("Bob");
+	ScavTrap	cp_scav(par_scav);
+
+	std::cout
+		<< "Default ScavTrap's name is "
+		<< def_scav.getName()
+		<< std::endl;
+
+	std::cout
+		<< "Parametrized ScavTrap's name is "
+		<< par_scav.getName()
+		<< std::endl;
+
+	std::cout
+		<< "Copied ScavTrap's name is "
+		<< cp_scav.getName()
+		<< std::endl;
+
+	par_scav.attack("Selena");
+	par_scav.takeDamage(20);
+
+	std::cout
+		<< par_scav.getName() << "'s HP = "
+		<< par_scav.getHp()
+		<< std::endl;
+
+	par_scav.beRepaired(10);
+
+	std::cout
+		<< par_scav.getName() << "'s HP = "
+		<< par_scav.getHp()
+		<< std::endl;
+
+	par_scav.guardGate();
+
+	ScavTrap	as_scav("Thomas");
+
+	as_scav = par_scav;
+	std::cout
+		<< "Thomas's name after assignation is "
+		<< cp_scav.getName()
+		<< std::endl;
+}
