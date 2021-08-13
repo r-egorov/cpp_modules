7071f81668b237e67bef77b866d0c326b11401e7
r-egorov <r-egorov@outlook.com>
2021-08-13 19:00:48 +0300

    [Day02]ex01 extend main.cpp

--- day_02/ex01/srcs/main.cpp
+++ day_02/ex01/srcs/main.cpp
@@ -15,6 +15,7 @@ int	main(void)
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
