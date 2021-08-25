35483d602c82bfa1233d316093f4f470cc2a4b75
Ruslan Egorov <r-egorov@outlook.com>
2021-08-25 17:33:52 +0300

    [Day05]ex00 done

---
+++ day_05/ex00/includes/Bureaucrat.hpp
@@ -0,0 +1,37 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+# include <iostream>
+
+class Bureaucrat
+{
+	private:
+		std::string const		name;
+		int						grade;
+
+	public:
+		Bureaucrat(void);
+		~Bureaucrat(void);
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(Bureaucrat const &obj);
+		Bureaucrat				&operator=(Bureaucrat const &obj);
+
+		std::string const		&getName(void) const;
+		int						getGrade(void) const;
+		void					incGrade(void);
+		void					decGrade(void);
+
+		class GradeTooHighException : public std::exception
+		{
+			const char	*what(void) const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			const char	*what(void) const throw();
+		};
+};
+
+std::ostream &	operator<<(std::ostream &o, Bureaucrat const &obj);
+
+#endif
