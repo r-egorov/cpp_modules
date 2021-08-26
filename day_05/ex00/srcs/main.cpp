bc77cb7ffaf8ba58a56d91f8bf15348135919357
Ruslan Egorov <r-egorov@outlook.com>
2021-08-26 11:16:03 +0300

    [Day05]ex00 fix main

--- day_05/ex00/srcs/main.cpp
+++ day_05/ex00/srcs/main.cpp
@@ -2,17 +2,21 @@
 
 int		main(void)
 {
+	Bureaucrat	bur("Thomas", 10);
+	std::cout << bur << std::endl;
+
+	Bureaucrat	bur1("Andrew", 140);
+	std::cout << bur1 << std::endl;
+
+	Bureaucrat	bur2("Kathy", 20);
+	bur2 = bur;
+	std::cout << bur2 << std::endl;
+
+	/*
+	 * TESTING TOO LOW EXCEPTION FOR A BUREAUCRAT
+	*/
 	try
 	{
-		Bureaucrat	bur("Thomas", 10);
-		std::cout << bur << std::endl;
-
-		Bureaucrat	bur1("Andrew", 140);
-		std::cout << bur1 << std::endl;
-
-		Bureaucrat	bur2("Kathy", 20);
-		bur2 = bur;
-		std::cout << bur2 << std::endl;
 
 		for (int i = 0; i < 20; i++)
 		{
@@ -20,16 +24,23 @@ int		main(void)
 			std::cout << "Decrementing Andrew's grade by 1" << std::endl;
 			std::cout << bur1 << std::endl;
 		}
+	}
+	catch (Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << "Grade is too low!" << std::endl;
+	}
+
+	/*
+	 * TESTING TOO HIGH EXCEPTION FOR A BUREAUCRAT
+	*/
+	try
+	{
 		for (int i = 0; i < 20; i++)
 		{
 			bur.incGrade();
 			std::cout << "Incrementing Thomas's grade by 1" << std::endl;
 			std::cout << bur << std::endl;
 		}
-	}
-	catch (Bureaucrat::GradeTooLowException &e)
-	{
-		std::cerr << "Grade is too low!" << std::endl;
 	}
 	catch (Bureaucrat::GradeTooHighException &e)
 	{
