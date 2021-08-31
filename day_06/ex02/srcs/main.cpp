c52f74e2e36b593fb4a2bfbf40c84fe4a2b7853a
Ruslan Egorov <r-egorov@outlook.com>
2021-08-31 15:37:09 +0300

    [Day06]ex02 done

---
+++ day_06/ex02/srcs/main.cpp
@@ -0,0 +1,19 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base	*generate(void);
+void	identify(Base *p);
+void	identify(Base &p);
+
+int		main(void)
+{
+	Base	*base = generate();
+	Base	&ref = *base;
+
+	identify(base);
+	identify(ref);
+
+	delete base;
+}
---
+++ day_06/ex02/srcs/utils.cpp
@@ -0,0 +1,94 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <string>
+
+enum	Classes
+{
+	CLASS_A,
+	CLASS_B,
+	CLASS_C
+};
+
+Base	*generate(void)
+{
+	unsigned int	seed;
+	int				coin;
+
+	seed = time(0);
+	srand(seed);
+	coin = rand() % 3;
+
+	std::cout << "[Generator]: ";
+	switch (coin)
+	{
+		case CLASS_A:
+		{
+			std::cout << "Generated class A" << std::endl;
+			return (new A());
+		}
+		case CLASS_B:
+		{
+			std::cout << "Generated class B" << std::endl;
+			return (new B());
+		}
+		case CLASS_C:
+		{
+			std::cout << "Generated class C" << std::endl;
+			return (new C());
+		}
+	}
+	return (nullptr);
+}
+
+void	identify(Base *p)
+{
+	std::cout << "[Identifier(ptr)]: ";
+	A	*a_ptr = dynamic_cast<A *>(p);
+	if (a_ptr)
+	{
+		std::cout << "A" << std::endl;
+		return ;
+	}
+
+	B	*b_ptr = dynamic_cast<B *>(p);
+	if (b_ptr)
+	{
+		std::cout << "B" << std::endl;
+		return ;
+	}
+
+	C	*c_ptr = dynamic_cast<C *>(p);
+	if (c_ptr)
+	{
+		std::cout << "C" << std::endl;
+		return ;
+	}
+}
+
+void	identify(Base &p)
+{
+	std::cout << "[Identifier(rfr)]: ";
+	A	*a_ptr = dynamic_cast<A *>(&p);
+	if (a_ptr)
+	{
+		std::cout << "A" << std::endl;
+		return ;
+	}
+
+	B	*b_ptr = dynamic_cast<B *>(&p);
+	if (b_ptr)
+	{
+		std::cout << "B" << std::endl;
+		return ;
+	}
+
+	C	*c_ptr = dynamic_cast<C *>(&p);
+	if (c_ptr)
+	{
+		std::cout << "C" << std::endl;
+		return ;
+	}
+}
