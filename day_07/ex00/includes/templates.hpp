d469454515b9b4b10ded79cb379c235d37212228
Ruslan Egorov <r-egorov@outlook.com>
2021-09-14 17:37:38 +0300

    [Day07]ex00 refactor

---
+++ day_07/ex00/includes/templates.hpp
@@ -0,0 +1,28 @@
+#ifndef TEMPLATES_HPP
+# define TEMPLATES_HPP
+
+template <typename T>
+void	swap(T &a, T &b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T		min(T &a, T &b)
+{
+	if (a < b)
+		return a;
+	return b;
+}
+
+template <typename T>
+T		max(T &a, T &b)
+{
+	if (a > b)
+		return a;
+	return b;
+}
+
+#endif
